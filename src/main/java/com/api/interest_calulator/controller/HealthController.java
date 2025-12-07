4cf07ebe16c1ead244db66c7a8e8d12e24ff9d8a
surajkhan pinjar <surajkhanpinjar@gmail.com>
2025-12-07 13:34:34 +0530

    Health End Point

--- src/main/java/com/api/interest_calulator/controller/HealthController.java
+++ src/main/java/com/api/interest_calulator/controller/HealthController.java
@@ -3,9 +3,11 @@ package com.api.interest_calulator.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/interest")
 public class HealthController {
 
     @Operation(summary = "Health Check Endpoint", description = "Used by RapidAPI and uptime monitors")
